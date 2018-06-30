10f0b747455b94ef448d256b73c35e303cfeb529
Celine O'Neil <celineloneil@gmail.com>
2018-07-01 00:26:01 +0200

    Sort todo priorities

--- .emacs.d/org-init.el
+++ .emacs.d/org-init.el
@@ -7,3 +7,19 @@
 (setq org-global-properties
       '(("Effort_ALL" .
          "0:05 0:15 0:30 0:45 1:00 2:00 6:00 10:00")))
+
+(use-package org-super-agenda
+  :config
+  (org-super-agenda-mode 1)
+  (setq org-super-agenda-groups
+        '((:name "emails"
+                 :todo "CHECKIN")
+          (:and (:effort< "0:29" :deadline past))
+          (:and (:effort< "0:29" :deadline today))
+          (:and (:effort< "0:59" :deadline past))
+          (:and (:effort< "0:59" :deadline today))
+          (:deadline past)
+          (:deadline today)
+          (:and (:effort< "0:29" :deadline future))
+          (:and (:effort< "0:59" :deadline future))
+          (:deadline future))))
