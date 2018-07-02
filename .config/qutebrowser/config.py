694d2d5f343df3e13ccdeef8d1c0898c33025c02
Celine O'Neil <celineloneil@gmail.com>
2018-07-02 13:21:05 +0200

    Configure qutebrowser completion

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -25,6 +25,10 @@ c.fonts.monospace = 'Fantasque Sans Mono'
 
 # completion
 c.completion.show = 'auto'
+c.completion.shrink = True
+c.completion.height = 65
+c.completion.scrollbar.width = 0
+c.fonts.completion.category = '10pt monospace'
 
 # statusbar
 c.statusbar.hide = True
@@ -55,7 +59,12 @@ color_mapping = {
         'colors.tabs.odd.fg',
         'colors.tabs.even.fg',
         'colors.tabs.selected.odd.bg',
-        'colors.tabs.selected.even.bg'
+        'colors.tabs.selected.even.bg',
+        'colors.completion.fg',
+        'colors.completion.item.selected.bg',
+        'colors.completion.item.selected.border.bottom',
+        'colors.completion.item.selected.border.top',
+        'colors.completion.category.fg'
     ],
     colors['special']['background']: [
         'colors.statusbar.normal.bg',
@@ -65,11 +74,18 @@ color_mapping = {
         'colors.tabs.odd.bg',
         'colors.tabs.even.bg',
         'colors.tabs.selected.odd.fg',
-        'colors.tabs.selected.even.fg'
+        'colors.tabs.selected.even.fg',
+        'colors.completion.item.selected.fg',
+        'colors.completion.odd.bg',
+        'colors.completion.even.bg',
+        'colors.completion.category.bg',
+        'colors.completion.category.border.bottom',
+        'colors.completion.category.border.top'
     ],
     colors['colors']['color1']: [
         'colors.statusbar.url.success.https.fg',
-        'colors.hints.match.fg'
+        'colors.hints.match.fg',
+        'colors.completion.match.fg'
     ],
     colors['colors']['color3']: [
         'colors.statusbar.url.hover.fg'
